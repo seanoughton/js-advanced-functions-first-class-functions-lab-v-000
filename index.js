7e9fd42c28f6a31d903a59de2fb32bfeee4b24f5
Sean Oughton <seanoughton@gmail.com>
2018-06-27 19:26:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ fareDoubler(5);
 
 //selectDifferentDrivers() — This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
 
-const selectDifferentDrivers = function (drivers) {
-  return drivers.slice(0, 2);
+const selectDifferentDrivers = function (drivers,selector) {
+  return selector(drivers);
 };
