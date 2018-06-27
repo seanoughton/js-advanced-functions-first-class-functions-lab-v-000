9babe0ccacb3da04f3b7bd1943e7610e5558f006
Sean Oughton <seanoughton@gmail.com>
2018-06-27 18:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
-  const myFunc = function() {
+  return const myFunc = function() {
     return drivers.slice(0, 2);
   }
   //return drivers.slice(0, 2);
