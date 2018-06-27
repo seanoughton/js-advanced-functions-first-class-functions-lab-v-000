1813994f63e716e81a093c6fcce0e9023050f891
Sean Oughton <seanoughton@gmail.com>
2018-06-27 19:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const returnLastTwoDrivers = function (drivers) {
 const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
 function createFareMultiplier(multiplier) {
-  //returns a function that will multiply a fare for a ride accordingly. If createFareMultiplier() receives an argument of 4, it will return a function that takes in a fare as an argument and quadruples the fare.
   return function(fare) {
     return fare * multiplier
   };
