6e257a90091f3acb80b3e831f3c34679593339ab
Sean Oughton <seanoughton@gmail.com>
2018-06-27 19:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ function createFareMultiplier(multiplier) {
     return fare * multiplier
   };
 };
+
+//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareDoubler() function accepts a fare as its lone argument and doubles it.
