1fde32c202b73cb8ca59fa79acc94cf7769d26f8
Sean Oughton <seanoughton@gmail.com>
2018-06-27 18:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
-  const something = function (drivers) {
-    return drivers[0,1]
-  };
-  return drivers[0];
+  for (const element of drivers) {
+    return element
+  }
 }
